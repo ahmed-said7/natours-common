d0418b1f1cbe995a1e5ea3494872c136799f71aa
ahmed-said7 <engahmedsaid125@gmail.com>
2024-02-16 00:25:36 +0200

    1st commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-export * from "./apiError";
-export * from "./apiFactory";
-export * from "./apiFeatures";
-export * from "./auth";
-export * from "./errorHandler";
-export * from "./nodemailer";
-export * from "./validationHandler";
-// export {
-//     apiError,apiFactory,apiFeatures,protect,
-//     allowedTo,errorHandler
-//     ,createTransport,validationHandler
-// }
+import {apiError} from "./apiError";
+import {apiFactory} from "./apiFactory";
+import {apiFeatures} from "./apiFeatures";
+import {allowedTo,protect} from "./auth";
+import {errorHandler} from "./errorHandler";
+import {createTransport} from "./nodemailer";
+import {validationHandler} from "./validationHandler";
+
+module.exports={
+    apiError,apiFactory,apiFeatures,protect,
+    allowedTo,errorHandler
+    ,createTransport,validationHandler
+}
